fe1304401f34448ebaf987f46815a92322aea4da
Peter Peev <peev.petar@gmail.com>
2019-12-04 19:00:05 +0200

    Kamino and One more

---
+++ KaminoFactory/KaminoFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace KaminoFactory
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	class KaminoFactory
+	{
+		static void Main(string[] args)
+		{
+			var arrayLength = int.Parse(Console.ReadLine());
+			var arrayDnaSequence = Console.ReadLine();
+			var arra = new int[arrayLength];
+			var listOfArrays = new List<Array>();
+			var countOfSequences = 0;
+
+			while (arrayDnaSequence != "Clone them!")
+			{
+				arra = arrayDnaSequence.Split('!', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+					.ToArray();
+				listOfArrays.Add(arra);
+				countOfSequences++;
+				arrayDnaSequence = Console.ReadLine();
+
+			}
+
+			foreach (var array in listOfArrays)
+			{
+				foreach (var element in array)
+				{
+					//var temp = string.Join(' ', element);
+					Console.Write($"{element} ");
+				}
+
+				Console.WriteLine();
+			}
+
+			
+		}
+	}
+}
---
+++ MagicSum/MagicSum.cs
@@ -0,0 +1,32 @@
+﻿namespace MagicSum
+{
+	using System;
+	using System.Linq;
+
+	class MagicSum
+	{
+		static void Main()
+		{
+			var inputArr = Console.ReadLine()
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			var inputNumber = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < inputArr.Length; i++)
+			{
+				for (int j = i+1; j < inputArr.Length; j++)
+				{
+					if (inputArr[i]+inputArr[j] == inputNumber)
+					{
+						Console.WriteLine($"{inputArr[i]} {inputArr[j]} ");
+					}
+				}
+			}
+
+
+
+		}
+	}
+}
