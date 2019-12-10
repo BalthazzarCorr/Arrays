a40e4d96bda39e5745a605a4da6db53ca146e246
Peter Peev <peev.petar@gmail.com>
2019-12-10 19:54:52 +0200

    new addition to the array exercies :)

--- KaminoFactory/KaminoFactory.cs
+++ KaminoFactory/KaminoFactory.cs
@@ -1,7 +1,6 @@
 ﻿namespace KaminoFactory
 {
 	using System;
-	using System.Collections.Generic;
 	using System.Linq;
 
 	class KaminoFactory
@@ -11,31 +10,82 @@
 			var arrayLength = int.Parse(Console.ReadLine());
 			var arrayDnaSequence = Console.ReadLine();
 			var arra = new int[arrayLength];
-			var listOfArrays = new List<Array>();
-			var countOfSequences = 0;
+			var DnaArray = new int[arrayLength];
+			var leftMostIndex = 0;
+			var sumOfDna = 0;
+			var lenghtOfSubSequence = 0;
+			var numberOfTheSequence = 0;
+			var numberOfCurrentSequence = 0;
 
 			while (arrayDnaSequence != "Clone them!")
 			{
 				arra = arrayDnaSequence.Split('!', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
 					.ToArray();
-				listOfArrays.Add(arra);
-				countOfSequences++;
+
+
+				numberOfCurrentSequence++;
+
+
+				int currentSum = 0;
+
+				for (int i = 0; i < arra.Length; i++)
+				{
+					if (arra[i] == 1)
+					{
+						currentSum++;
+					}
+				}
+
+				int currentLength = 0;
+				int currentIndex = 0;
+
+				for (int i = 0; i < arra.Length; i++)
+				{
+					if (arra[i]==1)
+					{
+
+						currentLength++;
+						if (currentLength == 1 )
+						{
+							currentIndex = i;
+						}
+
+						if (currentLength > lenghtOfSubSequence || currentLength == lenghtOfSubSequence && (currentIndex < leftMostIndex || currentSum > sumOfDna))
+						{
+							lenghtOfSubSequence = currentLength;
+							leftMostIndex = currentIndex;
+							numberOfTheSequence = numberOfCurrentSequence;
+							DnaArray = arra;
+							sumOfDna = currentSum;
+
+						}
+
+					}
+					else
+					{
+						currentIndex = 0;
+						currentLength = 0;
+					}
+
+					
+				}
+
+
+
 				arrayDnaSequence = Console.ReadLine();
 
 			}
 
-			foreach (var array in listOfArrays)
-			{
-				foreach (var element in array)
-				{
-					//var temp = string.Join(' ', element);
-					Console.Write($"{element} ");
-				}
 
-				Console.WriteLine();
+			if (numberOfTheSequence == 0)
+			{
+				numberOfTheSequence = 1;
 			}
 
-			
+			Console.WriteLine($"Best DNA sample {numberOfTheSequence} with sum: {sumOfDna}.");
+			Console.WriteLine(string.Join(" ", DnaArray));
+
 		}
+
 	}
 }
